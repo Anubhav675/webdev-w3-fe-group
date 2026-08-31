004ed401326be230926efc5b4e0d9cc8874f6484
Anubhav <anubhav675@gmail.com>
2026-09-01 00:29:42 +0300

    feat: render lists in React

---
+++ react-lists-lab1/src/App.jsx
@@ -0,0 +1,24 @@
+import Book from './Book';
+import booksData from './booksData';
+import './Book.css';
+
+// import Book from './Book';
+// import booksData from './booksData';
+// import './Book.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Book List</h1>
+      <div className="book-list">
+        {
+        booksData.map(
+          (book) => (
+          <Book key = {book.id} book={book} />))
+        }
+      </div>
+    </div>
+  );
+}
+
+export default App;
